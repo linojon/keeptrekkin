ec82d894db4ea88ab90ad3c8262ef73a2a687278
Jonathan Linowes <linojon@gmail.com>
2014-06-14 12:50:25 -0400

    rename config.app_title

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,6 @@ module Nh4k
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.app_name = 'My 4000 Footers'
+    config.app_title = 'My 4000 Footers'
   end
 end
