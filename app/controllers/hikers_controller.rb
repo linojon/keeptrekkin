e71b3db533ec94be736bfdba8bbc8453c3765d46
Jonathan Linowes <linojon@gmail.com>
2014-06-25 01:08:22 -0400

    fix typo

--- app/controllers/hikers_controller.rb
+++ app/controllers/hikers_controller.rb
@@ -3,13 +3,13 @@ class HikersController < ApplicationController
   expose(:trip)
   expose(:hiker, ancestor: :trip )
 
-  def create
-    if hiker = Hiker.where( email: params[:email] ).first
-      # already exists with this email, just use it, ignore name
-      trip.hikers << hiker
-      # [handle uniq exception]
-    else
+  # def create
+  #   if hiker = Hiker.where( email: params[:email] ).first
+  #     # already exists with this email, just use it, ignore name
+  #     trip.hikers << hiker
+  #     # [handle uniq exception]
+  #   else
 
-  end
+  # end
 
 end
