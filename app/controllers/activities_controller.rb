6abd86eb9c6d4153270dd9f09a3969f993988f3b
Jonathan Linowes <jonathan@parkerhill.com>
2014-09-07 15:43:44 -0400

    add link to hiker email msg

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,6 +3,6 @@ class ActivitiesController < ApplicationController
   skip_after_action :verify_policy_scoped
 
   def index
-    @activities = PublicActivity::Activity.order('created_at DESC')
+    @activities = PublicActivity::Activity.order('created_at DESC').page(params[:page]).per(100)
   end
 end
